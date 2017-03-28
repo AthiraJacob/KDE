0921883c3f05b42594365736572d0e396f868742
Athira Jacob <ajacob@dori>
2017-03-28 15:38:51 -0400

    ..

--- functions.py
+++ functions.py
@@ -6,63 +6,71 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from numpy.core.umath_tests import inner1d
-from scipy.misc import logsumexp
 
 class DataLoader():
 	'''
 	Data loader class that loads data, divides it into training, validation and testing sets and returns them
 	Also visualize the data
-	Param  dataset: name of the dataset (mnist/cifar-10)
+	Param  dataset : name of the dataset (mnist/cifar-10)
 	'''
-	def __init__(self,FLAGS):
-		#Initializes name of the dataset file
-		self.dataset = FLAGS.dataset
-		if FLAGS.dataset == 'mnist':
-			self.dataset_name = FLAGS.inputFolder + '/mnist.pkl'
-			print('Analyzing MNIST dataset!')
-		elif FLAGS.dataset == 'cifar-100':
-			self.dataset_name = [FLAGS.inputFolder + "cifar-100-python/train",FLAGS.inputFolder + "cifar-100-python/test"]
+	def __init__(self,dataset):
+		'''
+		Store dataset name and path in self. Assumes that the data files are stored in parent directory
+		'''
+		self.dataset = dataset
+		if dataset == 'mnist':
+			self.file_path = '../mnist.pkl'
+		elif dataset == 'cifar-100':
+			self.file_path = ['../train','../test']
 		else: 
 			print('Unrecognized dataset!')
 
-	def unpickle(self): 
+	def unpickle(self, file): 
 		#Unpickle the data file 
-		fo = open(self.dataset_name, 'rb')
-		dict = pickle.load(fo) #Object pickled in Python 2, unpickling now in python 3
+		fo = open(file, 'rb')
+		dict = pickle.load(fo) 
 		fo.close()
 		return dict
 
 	def load_data(self):
-		'''
-		Returns data split into 3 sub divisions: train, validation and testing
-		Eachs split is of the form: split = [data,labels]. Eg: split[0] gives N x d data matrix
-		'''
-		train,valid,test = self.unpickle()
-		N = 10000 #Use only 10k from trainset as training set, and another 10k as validation set
-		nSamp = train[0].shape[0]
+		#Load and unpickle datasets stored in self
+		if self.dataset == 'mnist':
+			train,valid,test = self.unpickle(self.file_path)
+			train = train[0]
+			test = test[0]
+		elif self.dataset == 'cifar-100':
+			train = self.unpickle(self.file_path[0])['data']
+			test = self.unpickle(self.file_path[1])['data']
+			train = train/float(np.max(train))
+			test = test/float(np.max(test))
+		#Use only 10k from trainset as training set, and another 10k as validation set
+		N = 10000
+		nSamp = train.shape[0]
 		indx = np.random.permutation(nSamp) #Shuffle
-		train_new = (train[0][indx[:N]],train[1][indx[:N]]) 
-		valid_new = (train[0][indx[N:2*N]],train[1][indx[N:2*N]])
-		return train_new,valid_new,test
+		return train[indx[:N]],train[indx[N:2*N]],test
 
 	def visualize(self,data, n = 20):
 		#Visualize first n*n images of the data in a nxn grid
 		plt.figure(figsize=(n,n))
 		gs1 = gridspec.GridSpec(n, n)
-		gs1.update(wspace=0.8, hspace=0.0005) # set the spacing between axes. 
+		gs1.update(wspace=0.008, hspace=0.05) # set the spacing between axes. 
 
-		for i in range(n*n):
-			img = data[0][i].reshape((28, 28))
-			label = data[1][i]
-			ax = plt.subplot(gs1[i])
-			plt.axis('off')
-			plt.title(str(label))
-			plt.imshow(img, cmap='gray')
-		plt.show()
+		if self.dataset == 'mnist':
+			for i in range(n*n):
+				ax = plt.subplot(gs1[i])
+				plt.axis('off')
+				img = data[i].reshape((28,28))
+				plt.imshow(img,'gray')
+			plt.show()
+		elif self.dataset == 'cifar-100':
+			for i in range(n*n):
+				ax = plt.subplot(gs1[i])
+				plt.axis('off')
+				img = data[i].reshape((32,32,3),order = 'F')
+				img = np.transpose(img,(1,0,2))
+				plt.imshow(img)
+			plt.show()
 
-	def pre_process(self,X):
-		X = (X-np.min(X))
 
 
 def log_likelihood(X,D,sigma):
@@ -73,7 +81,6 @@ def log_likelihood(X,D,sigma):
 			  sigma: smoothing parameter
 	Outputs-  ll: mx1 array of log likelihoods log(p(x))
 	'''
-	
 	N = D.shape[0]
 	K = np.arange(1,N+1).reshape([1,N])
 	d = X.shape[1]
@@ -81,7 +88,7 @@ def log_likelihood(X,D,sigma):
 
 	ll = np.zeros([nSamp,1])
 	k = 0
-	batchSize = 1   #Number of samples to be calculated simultaneously 
+	batchSize = 50   #Number of samples to be calculated simultaneously 
 	while k<nSamp:
 		t = min(k+batchSize,nSamp)
 		x = X[k:t]
--- main.py
+++ main.py
@@ -16,39 +16,43 @@ from functions import *
 FLAGS = None
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default="mnist",help='Dataset to use: mnist or cifar-100')
-parser.add_argument('--inputFolder', type=str, default="/cis/home/ajacob/Documents/enlitic",help='Dataset to use: mnist or cifar-100')
 parser.add_argument('--visualize', type=int, default=0,help='Visualize? If yes, input number of images to show')
 FLAGS, unparsed = parser.parse_known_args()
 
 
 #Load and split data
-data_loader = DataLoader(FLAGS)
+data_loader = DataLoader(FLAGS.dataset)
 train,valid,test = data_loader.load_data()
 
-#Visualize?
-# if FLAGS.visualize is not 0: 
-# 	data_loader.visualize(train,n = FLAGS.visualize)
+if FLAGS.visualize is not 0: #Visualize?
+	data_loader.visualize(train,n = FLAGS.visualize)
 
-# nSamp_train = train[0].shape[0]  #Number of training samples
-# nSamp_valid = valid[0].shape[0]  #Number of validation samples
-# d = valid[0].shape[1]   #Number of features
+nSamp_train = train.shape[0]  #Number of training samples
+nSamp_valid = valid.shape[0]  #Number of validation samples
+d = valid.shape[1]   #Number of features
 
-# # Search for optimal sigma through grid search
-# # sigmas = np.array([0.05,0.08,0.1,0.2,0.5,1.0,1.5,2.0])
-# sigmas = np.array([1.0])
-# #Store results in a dictionry
-# results = dict()
+# Search for optimal sigma through grid search
+# sigmas = np.array([0.05,0.08,0.1,0.2,0.5,1.0,1.5,2])
+sigmas = np.array([0.05])
+#Store results in a dictionry
+results = dict()
 
-# for sigma_ in sigmas:
+
+for sigma_ in sigmas:
 	
-# 	print('Calculating for sigma = ' + str(sigma_) + '...')
-# 	#Find log likelihood
-# 	start = time.time()
-# 	ll = log_likelihood(valid[0],train[0],sigma_)
-# 	end = time.time()
-# 	timing = end - start
-# 	results[sigma_] = {'log-likelihood': np.mean(ll),'timing': timing}
+	#Find log likelihood
+	start = time.time()
+	ll = log_likelihood(valid[0:500],train,sigma_)
+	end = time.time()
+	timing = end - start
+	results[sigma_] = {'log-likelihood': np.mean(ll),'timing': timing}
 
-# print('Results: ')
-# print(results)
+print(results)
 
+best_ll = -np.inf
+for key,value in results.items():
+	if value['log-likelihood'] > best_ll:
+		best_ll = value['log-likelihood']
+		best_sigma = key
+
+print('Best sigma = ' + str(best_sigma) + ' with log_likelihood = ' + str(best_ll))
